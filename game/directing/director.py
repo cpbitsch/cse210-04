375603f0cbce8dfe0184a44fa50da1d77115fc6c
Twister1M <jakerunning@hotmail.com>
2022-06-08 21:39:24 -0600

    Update director.py

--- game/directing/director.py
+++ game/directing/director.py
@@ -1,7 +1,3 @@
-from game.casting.actor import Actor
-from game.casting.cast import Cast
-from game.casting.falling_object import FallingObject
-from game.casting.player import Player
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
 
@@ -9,20 +5,36 @@ class Director(VideoService,KeyboardService):
     """
     Creates the Director class that is used to control the game.  Inherits VideoService and KeyboardService.
     """
-    def __init__(self):
+    def __init__(self, keyboard_service, video_service):
+
         """
         Initializes the object and sets the attributes
         """
-        self.is_playing = True
 
-    def start_game(self):
+        self.keyboard_service = keyboard_service
+        self.video_service = video_service
+
+    def _start_game(self, cast):
+
         "Starts the game."
 
-    def do_inputs(self):
-        "Gets inputs from player."
+        self._video_service.open_window()
+        while self._video_service.is_open_window():
+            self._do_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
+        self._video_service.close_window()
 
-    def do_updates(self):
+    def _do_inputs(self, cast):
+
+        "Gets movement inputs from player."
+
+        player = cast.get_direction()
+
+    def _do_updates(self):
+
         "Updates game with inputs obtained."
 
-    def do_outputs(self):
+    def _do_outputs(self):
+
         "Outputs information to the player."
