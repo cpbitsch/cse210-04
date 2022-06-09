d9cd8a33507ee304032fc5686060585dac17cfc1
Twister1M <jakerunning@hotmail.com>
2022-06-08 21:49:55 -0600

    Update director.py

--- game/directing/director.py
+++ game/directing/director.py
@@ -17,7 +17,7 @@ class Director(VideoService,KeyboardService):
     def _start_game(self, cast):
 
         "Starts the game."
-        self._video_service.clear_buffer()
+
         self._video_service.open_window()
         while self._video_service.is_open_window():
             self._do_inputs(cast)
@@ -38,3 +38,8 @@ class Director(VideoService,KeyboardService):
     def _do_outputs(self):
 
         "Outputs information to the player, specifically creates the objects and character on the screen."
+
+        self._video_service.clear_buffer()
+        actors = cast.
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
